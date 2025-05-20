5c429371cc3308f894686693e3e88746a1163a02
joachimbang <joachimbangirahe.jb@gmail.com>
2025-05-21 00:38:58 +0300

    updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowerRouter } from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowerRouter>
     <App />
-    </BrowerRouter>
   </StrictMode>,
-);
+)
